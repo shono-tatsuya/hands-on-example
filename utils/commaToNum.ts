bd99ff550315d9cfd43c2c4926e159f3eb297c25
TatsuyaShono <55311305+TatsuyaShono@users.noreply.github.com>
2024-01-08 11:56:23 +0900

    test

--- utils/commaToNum.ts
+++ utils/commaToNum.ts
@@ -2,6 +2,6 @@
  * カンマ区切りの文字列を数値に変換します
  */
 export const commaToNum = (str: string) => {
-  const noCommaStr = str.replace(",", "");
+  const noCommaStr = str.replaceAll(",", ""); // 1つめのカンマのみが置換される replace() では数値比較ができないため replaceAll() に変更
   return Number(noCommaStr);
 };
